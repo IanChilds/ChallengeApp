2e41f081c24426148f694eac41a277c588fbdfb4
ChrisKnott <chrisknott@hotmail.co.uk>
2013-10-26 20:27:54 +0100

    Map stuff

--- src/com/challenge/FindChallengeActivity.java
+++ src/com/challenge/FindChallengeActivity.java
@@ -2,17 +2,26 @@ package com.challenge;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Binnie
- * Date: 26/10/13
- * Time: 11:44
- * To change this template use File | Settings | File Templates.
- */
-public class FindChallengeActivity extends Activity {
+public class FindChallengeActivity extends FragmentActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map);
+        SupportMapFragment fragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        GoogleMap map = fragment.getMap();
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(51.497149, -0.044589), 15.0f));
+        MarkerOptions m = new MarkerOptions();
+
+
+
+        map.addMarker(new MarkerOptions().position(new LatLng(51.497149, -0.044589)).title("Hello world"));
     }
 }
