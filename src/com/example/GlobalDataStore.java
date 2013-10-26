f6e6bb2605dd1946a77cb33f5a4d83ef0545edb2
Ian Childs <imbchilds@gmail.com>
2013-10-26 12:03:49 +0100

    Add globals class

---
+++ src/com/example/GlobalDataStore.java
@@ -0,0 +1,11 @@
+package com.example;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Binnie
+ * Date: 26/10/13
+ * Time: 12:02
+ * To change this template use File | Settings | File Templates.
+ */
+public class GlobalDataStore {
+}
